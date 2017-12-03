63bcc6af85ef022ce35a725df8e1b70c62b739db
Tiranid <ermakovichdmitriy@gmail.com>
2017-12-03 17:38:32 +0300

    added local_ip detection for spring;

--- src/main/java/by/tiranid/tracker/AppConfig.java
+++ src/main/java/by/tiranid/tracker/AppConfig.java
@@ -6,24 +6,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.net.InetAddress;
+
 @SpringBootApplication
 @EnableScheduling
 @Slf4j
 public class AppConfig {
 
-/*
-    // соответствует <multipart-config> в web.xml
-    @Bean
-    MultipartConfigElement multipartConfigElement() {
-        MultipartConfigFactory factory = new MultipartConfigFactory();
-        factory.setMaxFileSize("128KB");
-        factory.setMaxRequestSize("128KB");
-        return factory.createMultipartConfig();
-    }
-*/
 
-    public static void main(String[] args){
-        SpringApplication.run(AppConfig.class);
+    public static void main(String[] args) throws Exception {
+        InetAddress addr = InetAddress.getLocalHost();
+        String server_Ip = addr.getHostAddress();
+        String server_port = "8080";
+        String[] ipArg = {"--server.address=" + server_Ip, "--server.port=" + server_port};
+
+
+        log.info("\n\n" + "Loading on " + server_Ip + ":" + server_port + "\n");
+        SpringApplication.run(AppConfig.class, ipArg);
     }
 
 }
